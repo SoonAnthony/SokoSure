e63fa1db570134d28879679386436317c8eb63f0
vincentkyalomusembi <vincentkyalomusembi@users.noreply.github.com>
2026-07-09 12:37:41 +0300

    add the sms shortcode to allow users to send requests

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     AT_USERNAME: str   # Africa's Talking username
     AT_API_KEY: str    # Africa's Talking API key
+    AT_SHORTCODE: str = "42303"  # AT SMS shortcode
 
     class Config:
         env_file = ".env"
